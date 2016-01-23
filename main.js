1ab5261bbfcd790141004aef1e5d1891de6e2adc
Valentin Miculit <valsbox@gmail.com>
2016-01-22 17:22:13 -0700

    v1

--- main.js
+++ main.js
@@ -2,7 +2,20 @@
 angular.module("js7",[])
 
 angular.module("js7")
-	.controller("js7Troller",["$scope", function($scope) {
+	.controller("js7Troller",["$scope", "$filter", function($scope, $filter) {
+
+		$scope.lightBoxSwitch = false
+
+		$scope.showMetrics = function(event) {
+			$scope.lightBoxSwitch = true
+
+			$scope.time = $filter('date')(event.timeStamp, "mm:ss")
+			$scope.distanceScrolled = event.view.scrollY*100/document.documentElement.scrollHeight
+		}
+
+		$scope.closeLightbox = function(){
+			$scope.lightBoxSwitch = false
+		}
 
 
 	}])
